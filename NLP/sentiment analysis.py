6e68c8d717832bbffcd8b224dd1ac40dfa785854
Daniel Rich <danielalexanderrich@gmail.com>
2024-10-04 19:25:15 -0400

    Update sentiment analysis.py

--- NLP/sentiment analysis.py
+++ NLP/sentiment analysis.py
@@ -54,6 +54,62 @@ plt.show(block=True)
 
 
 
-###future work will include 
+# ## roberta LLM for comparison
+MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+
+def polarity_scores_roberta(example):
+    encoded_text = tokenizer(example, return_tensors='pt')
+    output = model(**encoded_text)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+    scores_dict = {
+        'roberta_neg': scores[0],
+        'roberta_neu': scores[1],
+        'roberta_pos': scores[2]
+    }
+    return scores_dict
+
+# vader on ex
+print(sia.polarity_scores(ex))
+# roberta on ex
+print(polarity_scores_roberta(ex))
+print(ex)
+# ex appears to be more or less positive so the LLM is better
+
+# run llm on sampled data set
+
+res = {}
+for i in range(len(sampled_df)):
+    row = sampled_df.iloc[i]
+    try:
+        text = row['Text']
+        myid = row['Id']
+        vader_result = sia.polarity_scores(text)
+        vader_result_rename = {}
+        for key, value in vader_result.items():
+            vader_result_rename[f"vader_{key}"] = value
+        roberta_result = polarity_scores_roberta(text)
+        both = {**vader_result_rename, **roberta_result}
+        res[myid] = both
+    except RuntimeError:
+        print(f'Broke for id {myid}')
+    if i % 100 == 0:
+        print(i)
+
+# this sample has 6 broken IDs, not bad
+
+results_df = pd.DataFrame(res).T
+results_df = results_df.reset_index().rename(columns={'index': 'Id'})
+results_df = results_df.merge(sampled_df, how='left')
+
+sns.pairplot(data=results_df,
+             vars=['vader_neg', 'vader_neu', 'vader_pos',
+                  'roberta_neg', 'roberta_neu', 'roberta_pos'],
+            hue='Score',
+            palette='tab10')
+plt.show(block=True)
 
 
